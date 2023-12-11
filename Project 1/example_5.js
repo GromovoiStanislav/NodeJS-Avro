18265fdfa9bb95f75ff365ee40475e390b8284e5
Gromovoi Stanislav <gromila12@rambler.ru>
2023-12-11 11:20:40 +0600

    Project 1

---
+++ Project 1/example_5.js
@@ -0,0 +1,34 @@
+const avro = require('avsc');
+
+// Определение схемы Avro
+const schema = {
+  type: 'record',
+  name: 'Visit',
+  fields: [
+    { name: 'city', type: 'string' },
+    { name: 'zipCodes', type: { type: 'array', items: 'string' } },
+    { name: 'visits', type: 'int' },
+  ],
+};
+
+// Создание Avro схемы
+//const type = avro.Type.forSchema(schema);
+
+// Создание Avro схемы из образца данных
+const type = avro.Type.forValue({
+  city: 'Cambridge',
+  zipCodes: ['02138', '02139'],
+  visits: 2,
+});
+
+// Пример массива закодированных данных
+const bufs = [
+  type.toBuffer({ city: 'Seattle', zipCodes: ['98101'], visits: 3 }),
+  type.toBuffer({ city: 'NYC', zipCodes: [], visits: 0 }),
+  type.toBuffer({ city: 'Cambridge', zipCodes: ['02138', '02139'], visits: 2 }),
+];
+
+// Декодирование каждого буфера
+const decodedData = bufs.map((buf) => type.fromBuffer(buf));
+
+console.log('Decoded Data:', decodedData);
